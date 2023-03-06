d025f3596ef9b2bfea2a4a8353f30a280892af1c
Angelo Solis <88774848+angelosolis@users.noreply.github.com>
2023-03-06 22:13:18 +0800

    Add files via upload

---
+++ Fruits/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fruits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string to search: ");
+            string input = Console.ReadLine();
+            string filePath = @"C:\Users\solis\source\repos\Palindrome\Palindrome\bin\Debug\Fruits.txt";
+            bool isFound = false;
+
+
+            List<string> fruits = new List<string> {
+            "apple", "banana", "orange", "pineapple", "mango",
+            "strawberry", "grapefruit", "kiwi", "peach", "cherry"
+        };
+
+
+            if (fruits.Contains(input.ToLower()))
+            {
+                Console.WriteLine($"{input} is a fruit.");
+            }
+
+
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line.Contains(input))
+                    {
+                        isFound = true;
+                        break;
+                    }
+                }
+            }
+            if (isFound)
+            {
+                Console.WriteLine($"The input '{input}' is found in the file.");
+            }
+            else
+            {
+                Console.WriteLine($"The input '{input}' is not found in the file.");
+            }
+        }
+    }
+}
